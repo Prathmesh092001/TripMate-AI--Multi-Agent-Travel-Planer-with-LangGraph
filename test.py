56ea9ab4a5934b6c440f803bc7730d3b4978292f
Prathmesh092001 <prathmesh.22msd7035@vitapstudent.ac.in>
2026-08-09 13:19:20 +0530

    UI & Backend added

--- test.py
+++ test.py
@@ -1,8 +1,19 @@
 from tools.tavily_tool import tavily_search
 from tools.flight_tool import search_flights
+from backend import run_travel_agent
 
-res = search_flights("Plan a 7 days Switzerland trip from India")
-print(res)
+#res = search_flights("Plan a 7 days Switzerland trip from India")
+#print(res)
 
 #res = tavily_search("Best places to visit in Paris")
 #print(res)
+
+user_input = input("Enter your travel request: ")
+
+response = run_travel_agent(
+    user_input=user_input,
+    thread_id="test_user"
+) 
+
+print("\nFINAL RESPONSE:\n")
+print(response["answer"])
