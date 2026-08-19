d41b3441569b1ecfe5dd8e6299f206a8155aabe0
Prathmesh092001 <prathmesh.22msd7035@vitapstudent.ac.in>
2026-08-19 11:54:22 +0530

    MCP added

--- backend.py
+++ backend.py
@@ -10,7 +10,7 @@ os.environ["REQUESTS_CA_BUNDLE"] = certifi.where()
 from typing import TypedDict, Annotated
 import operator
 import uuid
-
+import asyncio
 import psycopg
 from psycopg.rows import dict_row
 
@@ -25,6 +25,7 @@ from langchain_core.messages import (
 from langchain_groq import ChatGroq
 from tools.tavily_tool import tavily_search
 from tools.flight_tool import search_flights
+from mcp_client_test import tavily_mcp_search
 
 
 def get_database_url():
@@ -52,7 +53,7 @@ if not GROQ_API_KEY:
 # =========================
 
 llm = ChatGroq(
-    model="llama-3.3-70b-versatile",
+    model="openai/gpt-oss-120b",
     api_key=GROQ_API_KEY
 )
 
@@ -94,7 +95,8 @@ def flight_agent(state: TravelState):
 
 def hotel_agent(state: TravelState):
     query = f"Best hotels for {state['user_query']}"
-    hotel_results = tavily_search(query)
+    # hotel_results = tavily_search(query)
+    hotel_results = asyncio.run(tavily_mcp_search(query))
 
     return {
         "hotel_results": hotel_results,
